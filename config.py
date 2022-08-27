759d0e9361f01f25879a737ecfe2a7bfae37f861
Fabian3210 <fabian.rei@web.de>
2022-08-27 13:56:33 +0200

    changed server host

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 import os, time, torch
 TIME_STAMP = f"{time.localtime().tm_year}.{time.localtime().tm_mon}.{time.localtime().tm_mday}_{time.localtime().tm_hour}.{time.localtime().tm_min}"
 SAVE_PATH = os.path.join(os.getcwd(), "results", TIME_STAMP)
-if os.path.isdir(SAVE_PATH):
-    raise FileExistsError
-os.mkdir(SAVE_PATH)
+if not os.path.isdir(SAVE_PATH):
+    os.mkdir(SAVE_PATH)
+else:
+    print("Dir already exists!")
 
 # M1 GPU support
 print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
 print(f"Is MPS available? {torch.backends.mps.is_available()}")
 device = "mps" if torch.backends.mps.is_available() else "cpu"
-device="cpu"
+device = "cpu"
 print(f"Using device: {device}")
 
-SERVER_HOST = "localhost"
-SERVER_PORT = 9999
+SERVER_HOST = ""
+SERVER_PORT = 9995
 
